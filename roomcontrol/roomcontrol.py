ff9950ad99ea378343a6560a6defb7bfb89a9199
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-07-27 18:19:26 +0200

    Rearrange directories and add setup.py

---
+++ roomcontrol/roomcontrol.py
@@ -0,0 +1,40 @@
+import logging
+
+import click
+from flask import Flask
+
+from roomcontrol.services.main import main_service
+from roomcontrol.services.music import music_service
+from roomcontrol.services.light import light_service
+from roomcontrol.services.alarm import alarm_service
+
+
+@click.group(name='Room Control')
+@click.option('--debug', default=False, is_flag=True,
+              help='Show debugging logs')
+@click.pass_context
+def cli(ctx, debug):
+    logging.basicConfig(level=logging.INFO)
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+    ctx.obj['debug'] = debug
+
+
+@cli.command(short_help='Start the server')
+@click.pass_context
+def serve(ctx):
+    app = Flask(__name__)
+    app.config['DEBUG'] = ctx.obj['debug']
+    app.register_blueprint(main_service)
+    app.register_blueprint(music_service, url_prefix='/music')
+    app.register_blueprint(light_service, url_prefix='/light')
+    app.register_blueprint(alarm_service, url_prefix='/alarm')
+    app.run()
+
+
+def main():
+    cli(obj={})
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+import roomcontrol
+
+
+def read(filename):
+    with open(filename, 'r') as f:
+        return f.read()
+
+
+def requirements(requirements_filename):
+    reqs = parse_requirements(requirements_filename, session=False)
+    return [str(r.req) for r in reqs]
+
+
+DESCRIPTION = """
+Backend service designed to run on a RaspberryPi for the RoomControl app
+(https://github.com/miguelfrde/roomcontrol).
+"""
+
+
+setup(
+    name='roomcontrol',
+    version=roomcontrol.__version__,
+    url='https://github.com/miguelfrde/roomcontrol_backend',
+    license='MIT License',
+    author='Miguel Flores Ruiz de Eguino <miguelfrde>',
+    author_email='miguel.frde@gmail.com',
+    description=DESCRIPTION,
+    long_description=read('README.md'),
+    packages=find_packages(exclude=['test', 'test.*']),
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python 2.7',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 1 - Planning',
+        'Natural Language :: English',
+        'Environment :: Other Environment',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Utilities'
+    ],
+    entry_points={
+        'console_scripts': [
+            'roomcontrol = roomcontrol.roomcontrol:main'
+        ]
+    },
+    tests_require=requirements('requirements-dev.txt'),
+    install_requires=requirements('requirements.txt'),
+    extras_require={
+        'dev': requirements('requirements-dev.txt')
+    }
+)
