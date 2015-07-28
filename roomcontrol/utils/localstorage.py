e5b19b25540b19e6eb0e74164bc2bf1f06bcad82
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-07-28 22:44:10 +0200

    Add localstorage utility

---
+++ roomcontrol/utils/localstorage.py
@@ -0,0 +1,34 @@
+import configparser
+import os.path
+from functools import wraps
+
+
+class LocalStorage:
+    def __init__(self, filename):
+        self._filename = filename
+        self._config = configparser.ConfigParser()
+        if os.path.isfile(filename):
+            self._config.read(filename) 
+
+    def persist_changes(f):
+        @wraps(f)
+        def wrapper(self, *args, **kwargs):
+            f(self, *args, **kwargs)
+            with open(self._filename, 'w') as configfile:
+                self._config.write(configfile)
+        return wrapper
+
+    @persist_changes
+    def set(self, kind, key, value=None):
+        self._config[kind][key] = str(value)
+
+    @persist_changes
+    def set_all(self, kind, mapping):
+        m = {str(k): str(v) for k, v in mapping.items()}
+        self._config[kind] = m
+
+    def get(self, kind, key):
+        return self._config[kind][key]
+
+    def get_all(self, kind):
+        return self._config[kind]
---
+++ tests/test_localstorage.py
@@ -0,0 +1,32 @@
+from roomcontrol.utils.localstorage import LocalStorage
+import py.path
+import pytest
+
+
+TEST_FILE="""
+[kind1]
+a=1
+b=2
+
+[kind2]
+c=3
+"""
+
+
+@pytest.fixture
+def ls(tmpdir):
+    p = tmpdir.join('test_localstorage.in')
+    p.write(TEST_FILE)
+    obj = LocalStorage(str(p))
+    return obj
+
+
+def test_set_corresponds_to_get(ls):
+    ls.set('kind2', 'd', '4')
+    assert ls.get('kind2', 'd') == '4'
+
+
+def test_set_all_corresponds_to_get_all(ls):
+    data = {'e': '5', 'f': '6'}
+    ls.set_all('kind3', data)
+    assert ls.get_all('kind3') == data
