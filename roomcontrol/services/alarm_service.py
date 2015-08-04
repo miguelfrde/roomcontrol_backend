b105db0aef52748058fd8a8728e2d1500ddf8275
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-08-04 12:54:24 +0200

    Load default alarm values from storage

--- roomcontrol/services/alarm_service.py
+++ roomcontrol/services/alarm_service.py
@@ -1,27 +1,37 @@
 import time
 
 from nameko.events import event_handler
+from nameko.rpc import RpcProxy
 from nameko.timer import timer
 
 
 class AlarmService:
     name = 'alarm_service'
 
+    storage_rpc = RpcProxy('localstorage_service')
+
     def __init__(self):
-        self.hours = 0
-        self.minutes = 0
-        self.is_on = False
-        self.light_program = None
-        self.sound = None
+        defaults = self.storage_rpc.get_all('alarm')
+        t = defaults['hour']
+        self.hours = t.tm_hour
+        self.minutes = t.tm_min
+        self.is_on = self.on_str(defaults['active'])
+        self.light_program = defaults['light']
+        self.sound = defaults['sound']
+
+    def on_str(self, value):
+        if type(value) == str:
+            self.value = self.value.lower() in ('true', 'on')
+        if self.value is None:
+            return False
+        return value
 
     @event_handler("http_service", "alarm_settings_updated")
     def update_alarm(self, new_settings):
         t = time.localtime(new_settings['hour'])
         self.hours = t.tm_hour
         self.minutes = t.tm_min
-        self.is_on = new_settings['active']
-        if type(self.is_on) == str:
-            self.is_on = self.is_on.lower() in ('true', 'on')
+        self.is_on = self.on_str(new_settings['active'])
         self.light_program = new_settings['light']
         self.sound = new_settings['sound']
 
